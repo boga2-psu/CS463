58d9a8457b7c9203a208a2c7fb1bf85f1b360809
boga2-psu <boga2@pdx.edu>
2024-11-20 22:36:14 -0800

    Complete 05-destructuring.js

--- Notebook/07-javascript/05-destructuring.js
+++ Notebook/07-javascript/05-destructuring.js
@@ -1,6 +1,15 @@
 const destructureItems = (input) => {
   // destructure the name and parents of the character and format the output as shown
+  const {
+    first,
+    last,
+    allegiance: {
+      parents: { mother, father },
+    },
+  } = input;
 
+  // Format and return the output
+  input = `${first} ${last}\n  daughter of\n  ${mother} and ${father}`;
   return input;
 };
 
